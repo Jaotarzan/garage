d513e363bd6007329d12145663003e08e180430a
jaotarzan <joaovssouza59@gmail.com>
2025-04-22 19:29:35 -0300

    FEAT: Criando tabela de modelo

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from garagem.models import Acessorio, Cor
+from garagem.models import Acessorio, Cor, Modelo
 
 # Register your models here.
 
 admin.site.register(Acessorio)
 admin.site.register(Cor)
+admin.site.register(Modelo)
