60ffe86399845c127767917aa2baae0972efe9c0
jaotarzan <joaovssouza59@gmail.com>
2025-04-22 19:23:41 -0300

    FEAT: Criando tabela de cor

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from garagem.models import Acessorio
+from garagem.models import Acessorio, Cor
 
 # Register your models here.
 
 admin.site.register(Acessorio)
+admin.site.register(Cor)
